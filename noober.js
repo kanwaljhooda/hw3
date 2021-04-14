ff24b0177c6c5567b190d9d7ab3896f75e7abc34
K.J. Hooda <kj.hooda@kellogg.northwestern.edu>
2021-04-14 13:54:32 -0500

    HW 3 updates

--- noober.js
+++ noober.js
@@ -13,5 +13,45 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  // Parse the attributes of "ride" object and store each data element individually
+
+  let firstName = ride.passengerDetails.first
+  let lastName = ride.passengerDetails.last
+  let phoneNumber = ride.passengerDetails.phoneNumber
+  let pickupAddress = ride.pickupLocation.address
+  let pickupCity = ride.pickupLocation.city
+  let pickupState = ride.pickupLocation.state
+  let pickupZip = ride.pickupLocation.zip
+  let dropoffAddress = ride.dropoffLocation.address
+  let dropoffCity = ride.dropoffLocation.city
+  let dropoffState = ride.dropoffLocation.state
+  let dropoffZip = ride.dropoffLocation.zip
+  let numberOfRiders = ride.numberOfPassengers
+  let purpleRequest = ride.purpleRequested
+
+  // Define variable for ride type
+  let rideType
+
+  // If purple ride has been requested than set it to Purple
+  if (purpleRequest == true){
+    rideType = `Purple`
+  }
+
+  // If a purple ride is not requested but there are more than 3 passengers on a requested ride then type should be "XL" 
+  else if (numberOfRiders > 3) {
+    rideType = `XL`
+  }
+
+  // If neither purple ride is requested nor more than 3 passengers, then ride type should be "X" 
+  else {
+    rideType = `X`
+  }
+
+  // Create human-readable description of the ride
+  let output = `Noober ${rideType} Passenger: ${firstName} ${lastName} - ${phoneNumber}. Pickup at ${pickupAddress}, ${pickupCity}, ${pickupState} ${pickupZip}. Drop-off at ${dropoffAddress}, ${dropoffCity}, ${dropoffState} ${dropoffZip}.`
+
+  // Display human-readable description of the ride on the console
+  console.log(output)
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
